4b290937764f990ec5fdce214dc26faf0591e931
Zdenek Grof <zdenek.grof@vscht.cz>
2024-12-12 19:08:06 +0100

    Optimize day 12

--- src/day2412.f90
+++ src/day2412.f90
@@ -1,5 +1,6 @@
 module day2412_mod
   use parse_mod, only : read_pattern
+  use iso_fortran_env, only : dp => real64
   implicit none
 
   type garden_t
@@ -12,7 +13,7 @@ module day2412_mod
     integer, allocatable :: a(:,:)
     integer :: n=0
   end type
-  integer, parameter :: EDGE_COMPONENTS = 5
+  integer, parameter :: EDGE_COMPONENTS = 4
 
 contains
 
@@ -21,14 +22,18 @@ contains
 
     type(garden_t) :: garden
     integer :: ans1, ans2
+    real(dp) :: time0, time1
 
+    call cpu_time(time0)
     garden%map = read_pattern(file)
     call label_connected(garden)
     call count_fences(garden)
     ans1 = sum(garden%area * garden%peri)
     ans2 = sum(garden%area * garden%side)
+    call cpu_time(time1)
     print '("Ans 12/1 ",i0,l2)', ans1, ans1==1533024
     print '("Ans 12/2 ",i0,l2)', ans2, ans2==910066
+    print '("  elapsed time ",f6.3," s")', time1-time0
 
   end subroutine day2412
 
@@ -49,7 +54,6 @@ contains
       call crawl(i, j, this, id, this%map(i,j))
     end do
     end do
-print '("Labeled components ",i0)', id
 
     if (allocated(this%area)) deallocate(this%area)
     allocate(this%area(id))
@@ -62,73 +66,74 @@ print '("Labeled components ",i0)', id
   subroutine count_fences(this)
     type(garden_t), intent(inout) :: this
 
-    integer :: i, j, di, dj, dj2, di2
-    type(edgelist_t) :: edges
+    integer :: i, j, di, dj, dj2, di2, k, ncomp
+    type(edgelist_t), allocatable :: edges(:)
     integer :: edge(EDGE_COMPONENTS)
 
+    integer, parameter, dimension(*,*) :: DIR=reshape( &
+      [0, -1, -1, 0, 0, 1, 1, 0], [2,4])
+    integer, parameter :: ID_LEFT=1, ID_DOWN=2, ID_RIGHT=3, ID_UP=4
+    logical :: ngb_alien
 
-    ! Helper array "ngbs" - to store the number of alien neighbours
+    ncomp = maxval(this%id)
+
+    ! Perimeter of every region and helper array "ngbs"
+    ! - to store the number of alien neighbours
+    if (allocated(this%peri)) deallocate(this%peri)
+    allocate(this%peri(ncomp))
     if (allocated(this%ngbs)) deallocate(this%ngbs)
     allocate(this%ngbs(size(this%map,1),size(this%map,2)), source=4)
+
+    ! Number of sides for every region and helper array of edgelists
+    if (allocated(this%side)) deallocate(this%side)
+    allocate(this%side(ncomp))
+    allocate(edges(ncomp))
+
+    ! Loop over the map
     do i=1,size(this%ngbs,1)
     do j=1,size(this%ngbs,2)
-      do di=-1,1
-      do dj=-1,1
-        if (abs(di)+abs(dj)/=1) cycle
+      NGB_LOOP: do k=1,4
 
-        ! what the edge would look like?
+        di = DIR(1,k)
+        dj = DIR(2,k)
         dj2 = (dj-1)/2
         di2 = (di-1)/2
-        if (di==0) then
-          if (dj>0) then
-            ! ngb at right - vector heading down
-            edge = [i-1, j+dj2, i, j+dj2, this%id(i,j)]
-          else
-            ! ngb at left - vector heading up
-            edge = [i, j+dj2, i-1, j+dj2, this%id(i,j)]
-          end if
-        else
-          if (di>0) then
-            ! ngb is below - vector heading left
-            edge = [i+di2, j, i+di2, j-1, this%id(i,j)]
-          else
-            ! ngb is above - vector heading right
-            edge = [i+di2, j-1, i+di2, j, this%id(i,j)]
-          end if
-        end if
-
-        ! for each of four neighbours
         if (i+di<1 .or. j+dj<1 .or. i+di>size(this%ngbs,1) .or. &
             &                       j+dj>size(this%ngbs,2)) then
-          ! add edge if at the border of the map
-          call add_edge(edges, edge)
-        else if (this%map(i+di,j+dj)/=this%map(i,j)) then
-          ! add edge if neighbour is alien
-          call add_edge(edges, edge)
+          ngb_alien = .true.
         else
-          ! reduce the number of fences if neighbour has the same id
-          this%ngbs(i,j) = this%ngbs(i,j)-1
+          ngb_alien = this%map(i+di,j+dj)/=this%map(i,j)
         end if
+
+        if (.not. ngb_alien) then
+          this%ngbs(i,j) = this%ngbs(i,j)-1
+        else
+          select case(k)
+          case(ID_LEFT)
+            edge = [i, j+dj2, i-1, j+dj2]
+          case(ID_RIGHT)
+            edge = [i-1, j+dj2, i, j+dj2]
+          case(ID_DOWN)
+            edge = [i+di2, j, i+di2, j-1]
+          case(ID_UP)
+            edge = [i+di2, j-1, i+di2, j]
+          end select
+          call add_edge(edges(this%id(i,j)), edge)
+        end if
+
+      end do NGB_LOOP
+    end do
+    end do
+
+    do k=1, ncomp
+      this%peri(k) = sum(this%ngbs, mask=this%id==k)
+      do
+        i = edges(k)%n
+        call reduce_edges(edges(k))
+        if (edges(k)%n==i) exit
+!!      print '("Number of edges reduced from ",i0," to ",i0)',i,edges(k)%n
       end do
-      end do
-    end do
-    end do
-
-    if (allocated(this%peri)) deallocate(this%peri)
-    if (allocated(this%side)) deallocate(this%side)
-    allocate(this%peri(maxval(this%id)))
-    allocate(this%side(maxval(this%id)))
-
-    do
-      i = edges%n
-      call reduce_edges(edges)
-      if (edges%n==i) exit
-      print '("Number of edges reduced from ",i0," to ",i0)',i,edges%n
-    end do
-
-    do i=1, size(this%peri)
-      this%peri(i) = sum(this%ngbs, mask=this%id==i)
-      this%side(i) = count(edges%a(5,1:edges%n)==i)
+      this%side(k) = edges(k)%n
     end do
   end subroutine count_fences
 
@@ -137,15 +142,14 @@ print '("Labeled components ",i0)', id
     type(edgelist_t), intent(inout) :: this
 
     integer :: i, j
+    logical, allocatable :: active(:)
 
+    allocate(active(this%n), source= .true.)
     MAIN_LOOP: do i=1, this%n
       do j=1, this%n
 
         ! ignore removed edges
-        if (this%a(5,i) == 0 .or. this%a(5,j) == 0) cycle
-
-        ! proceed only with edges belongig to the same component id
-        if (this%a(5,i) /= this%a(5,j)) cycle
+        if (.not. (active(i) .and. active(j))) cycle
 
         ! both edges must be different
         if (i==j) cycle
@@ -163,8 +167,8 @@ print '("Labeled components ",i0)', id
           ! end point of "i" is same as beg point of "j"
           ! combine edges into "i" and remove "j"
           this%a(3:4,i)=this%a(3:4,j)
-          this%a(:,j) = 0
-          !exit MAIN_LOOP
+!         this%a(:,j) = 0
+          active(j) = .false.
         end if
       end do
     end do MAIN_LOOP
@@ -173,8 +177,9 @@ print '("Labeled components ",i0)', id
     i = 1
     do
       if (i>this%n) exit
-      if (all(this%a(:,i)==0)) then
+      if (.not. active(i)) then
         this%a(:,i) = this%a(:,this%n)
+        active(i) = active(this%n)
         this%n = this%n - 1
       else
         i = i + 1
@@ -239,6 +244,5 @@ print '("Labeled components ",i0)', id
     this%n = this%n + 1
     this%a(:,this%n) = edge
   end subroutine add_edge
-
 
 end module day2412_mod
